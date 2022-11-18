811db41b3235ba834553c543441996f3b2510da0
Drew White <d.white0002@gmail.com>
2022-11-18 10:25:52 -0800

    create test to test remove dups

---
+++ test_remove_dups.py
@@ -0,0 +1,14 @@
+import pytest
+from main import remove_dups
+
+@pytest.mark.parametrize("list1, expected_result", [
+([1, 3, 5], [1, 3, 5]), 
+([3, 9, 9], [3, 9]), 
+(["x", "y", "z"], ["x", "y", "z"]), 
+(["x", "y", "y"], ["x", "y"]),
+([], []),
+("hello", ["h", "e", "l", "o"])
+(None, TypeError)
+])
+def test_remove_dups(list1, expected_result):
+  assert remove_dups(list1) == expected_result
